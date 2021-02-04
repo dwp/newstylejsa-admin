9fe49d8ed6bc712dc820e732d4cb61ba960c11a8
Neil Holroyd <neil.holroyd@engineering.digital.dwp.gov.uk>
2021-02-04 11:24:28 +0000

    Update copy-assets.js

--- gulp/copy-assets.js
+++ gulp/copy-assets.js
@@ -8,8 +8,8 @@ const gulp = require('gulp')
 const config = require('./config/config.json')
 
 gulp.task('copy-assets', function () {
-  return gulp.src([config.paths.assets + '/**', '!' + config.paths.assets + 'sass{,/**/*}'
-  //  '!' + config.paths.js + '/**/*'])
+  return gulp.src([config.paths.assets + '/**', '!' + config.paths.assets + 'sass{,/**/*}',
+    '!' + config.paths.js + '/**/*'])
   .pipe(gulp.dest(config.paths.public))
 })
 
